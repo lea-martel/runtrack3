93fdf493313b48df2d8b1e7eab6ba8203aabd2df
lea-martel <lea.martel@laplateforme.io>
2020-09-08 09:34:45 +0200

    job 09

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -1,4 +1,11 @@
-function tri(numbers) {
-
-    var order = asc;
+function tri(nombres, order = 'asc')
+{
+	if (order === 'desc') {
+		return nombres.sort().reverse();
+	}
+	return nombres.sort();
+}
+window.onload = function()
+{
+	console.log(tri([1,4,7,0,8,2]));
 }
