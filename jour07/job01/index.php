6407b5570cc4a9ce71f42840bf22d052ef1ad62d
lea-martel <lea.martel@laplateforme.io>
2020-09-15 19:49:19 +0200

    job01

--- jour07/job01/index.php
+++ jour07/job01/index.php
@@ -190,6 +190,6 @@
                             </div>
                         </div>
                     </form>
-                </div>
+            </div>
     </main>
 </body>
--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=yes" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -16,10 +16,11 @@
     <main>
         <div class="container-fluid bg-primary" style="height:150px;"> </div>
 
-        <div class="container bg-primary" style="height:150px; margin-top:10px;">
-        <div class="row">
-  <div class="col-sm">col-sm</div>
-  <div class="col-sm">col-sm</div>
-  <div class="col-sm">col-sm</div>
-</div>
-</div>
+        <div class="container-fluid">
+            <div class="row" style="height: 5vh;">
+                <div class="col-4 border border-danger">
+                </div>
+                <div class="col-4 border border-succes bg-primary"></div>
+                <div class="col-4 border border-warning bg-primary"></div>
+            </div>
+        </div>
