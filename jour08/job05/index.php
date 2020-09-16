c365b985d5ec63d8e01c5b806f6ade23fc7ce2d7
lea-martel <lea.martel@laplateforme.io>
2020-09-16 13:29:14 +0200

    job05

---
+++ jour08/job05/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=yes" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <title>Materialize</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+
+      <div class="nav-wrapper red lighten-1"> 
+      <img src="LOGO-ENCART.png" style="width:3%;">
+        <a href="#" class="brand-logo"></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Inscription</a></li>
+          <li><a href="#">Connexion</a></li>
+          <li><a href="#">Rechercher</a></li>
+        </ul>
+    </div>
+      </div>
+    </nav>
+  </header>
+  <main>
+<div class="container">
+  <div class="row">
+    <div class="col s6">
+      <div class="card">
+        <div class="card-image">
+          <img src="cup-of-coffee-1280537_1920.jpg">
+          <span class="card-title">CAFÉ EXPRESSO</span>
+        </div>
+        <div class="card-content">
+          <p>Expresso, c’est l’expresso nouvelle génération. La capsule, véritable chambre d’extraction, contient 6,7g de cafés 100% Arabica déjà tassés, 
+              pour un authentique espresso italien, en toute simplicité.</p>
+        </div>
+        <div class="card-action">
+          <a href="https://adbeus.com/">Commander en ligne</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s6">
+      <div class="card">
+        <div class="card-image">
+          <img src="coffee-171653_1920.jpg">
+          <span class="card-title">CAFÉ GRAIN</span>
+        </div>
+        <div class="card-content">
+          <p>Toute la gamme de café en grains, assemblage illy ou Monoarabica. Idéal pour ceux qui souhaitent préparer leur café préféré en commençant par moudre le grain. Il suffit d'un moulin à café ou d'une machine à café automatique!</p>
+        </div>
+        <div class="card-action">
+          <a href="https://adbeus.com/">Commander en ligne</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="coffee-4618705_1920.jpg">
+          <span class="card-title">DÉCAFÉINÉ</span>
+        </div>
+        <div class="card-content">
+          <p>Pour notre café, nous employons les meilleures techniques d'extraction de la caféine. Retrouvez le goût inimitable du café illy et tous ses arômes avec moins de 0,05% de caféine.</p>
+        </div>
+        <div class="card-action">
+          <a href="https://adbeus.com/">Commander en ligne</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="cappuccino-2179028_1920.jpg">
+          <span class="card-title">CAPPUCIONO</span>
+        </div>
+        <div class="card-content">
+          <p>Le cappuccino est un café, servi dans une grande tasse, à base d'un expresso et coiffé de lait préalablement chauffé à la vapeur jusqu'à le faire mousser. </p>
+        </div>
+        <div class="card-action">
+          <a href="https://adbeus.com/">Commander en ligne</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="beverage-1869598_1920.jpg">
+          <span class="card-title">CAFÉ NOIR</span>
+        </div>
+        <div class="card-content">
+          <p>Pour ceux qui préfèrent un volume de café plus important, laissez-vous tenter parmi un choix de capsules et de cafés moulus adaptés à ces préparations XL !</p>
+        </div>
+        <div class="card-action">
+          <a href="https://adbeus.com/">Commander en ligne</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  </main>
+  <footer>
+  <footer class="page-footer red lighten-1">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer</h5>
+                <p class="grey-text text-lighten-4">Formulaire de création de compte</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Accueil</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Inscription</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Connexion</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Rechercher</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+  </footer>
+</body>
+</html>
